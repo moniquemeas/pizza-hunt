035634849246de2c265caed30c7976f42b40fdde
Monique <monique.meas@gmail.com>
2022-10-04 01:27:33 -0400

    added comment model and routes

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -6,6 +6,12 @@ const pizzaController = {
 
   getAllPizza(req, res) {
     Pizza.find({})
+    .populate({
+      path: 'comments',
+      select: '-__v'
+    })
+    .select('-__v')
+    .sort({ _id: -1 })
     .then(dbPizzaData => res.json(dbPizzaData))
     .catch(err => {
         console.log(err);
@@ -17,6 +23,11 @@ const pizzaController = {
 
   getPizzaById({params}, res) {
     Pizza.findOne({_id: params.id})
+    .populate({
+      path: 'comments',
+      select: '-__v'
+    })
+    .select('-__v')
     .then(dbPizzaData => {
         //if no pizza is found, send 404
         if(!dbPizzaData) {
